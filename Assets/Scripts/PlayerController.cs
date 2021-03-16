e185b34c1f77f5cc4dcb63486158dcefa5255e3f
Shaun Norton <shaunnorton87@gmail.com>
2021-03-16 11:01:06 -0700

    Player can now aim with the mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,16 +22,34 @@ public class PlayerController : MonoBehaviour
 
     public void OnFire()
     {
-        PlayerRB.AddForce(Vector3.up * 90);
+
+        Vector3 newVector = Vector3.forward;
+        PlayerRB.AddForce(Vector3.up);
     }
     public void OnMove(InputValue input)
     {
         Vector2 inputValue = input.Get<Vector2>();
+        PlayerRB.AddForce(new Vector3(-inputValue.x, Player.transform.position.y, -inputValue.y)*30);
+        Debug.Log(new Vector3(inputValue.x, Player.transform.position.y, inputValue.y) * 30);
     }
     public void OnLook(InputValue input)
     {
-        Debug.Log(input.Get<Vector2>());
-        Player.transform.LookAt(input.Get<Vector2>());
+        Vector2 LookVector = input.Get<Vector2>();
+        Vector3 LookPoint;
+
+        Plane plane = new Plane(Vector3.up,Player.transform.position);
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(LookVector);
+        if (plane.Raycast(ray,out distance))
+        {
+            LookPoint = ray.GetPoint(distance);
+            Player.transform.LookAt(LookPoint);
+            /*Debug.Log(LookPoint);*/
+        }
+
+
+        /*Debug.Log(input.Get<Vector2>());*/
+        
     }
 }
 
